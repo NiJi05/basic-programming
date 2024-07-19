5d4b92faa5d6b5fc3a1540d1915a109bbdbf746f
NiJi05 <cxzza147258369@gmail.com>
2024-07-19 16:22:38 +0700

    ส่งงาน

--- ep1/index.ts
+++ ep1/index.ts
@@ -1,11 +1,11 @@
 const firstName:string ='กิตติพงศ์ แซ่ถัง'
 let y: number = 10
 y = 20
-let ต้องการกลับบ้าน: boolean = true
+let goHome: boolean = true
 
 console.log(firstName)
 console.log(y)
-console.log(ต้องการกลับบ้าน)
+console.log(goHome)
  
 const a: number = 28
 const b: number = 50
