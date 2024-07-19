d3eeca0d33635fd346ae10cd557c8424c49eca8a
NiJi05 <cxzza147258369@gmail.com>
2024-07-19 16:19:46 +0700

    เปรียบเทียบค่า

--- ep1/index.ts
+++ ep1/index.ts
@@ -6,12 +6,12 @@ let ต้องการกลับบ้าน: boolean = true
 console.log(firstName)
 console.log(y)
 console.log(ต้องการกลับบ้าน)
-
+ 
 const a: number = 28
 const b: number = 50
 
-console.log(a===b)
+console.log(a === b)
 
-console.log(a<b)
+console.log(a > b)
 
-console.log(a>=b)
+console.log(a <= b)
