7c6fd7f6bec5d9b5fe5abf0718907490f7ed8963
NiJi05 <cxzza147258369@gmail.com>
2024-09-05 23:59:32 +0700

    การบ้าน2

---
+++ การบ้าน/inde.ts
@@ -0,0 +1,34 @@
+const me = {
+    firstName: 'kittipong',
+    lastName: 'satrg',
+    age: 19,
+    cam: 99/99,
+    swime: 88,
+    road: 'kingdom',
+    zone: 'area51',
+    county: 'Dark Knight',
+    province: 'Brave',
+    zipcode: 12080,
+    email: 'suuuu@gmail.com',
+    mobilephone: 'rog',
+    phonenumber: 911,
+
+    
+
+}
+
+
+// ประวัติส่วนตัว
+
+// ชื่อ: kittipong
+// นามสกุล: satrg
+// อายุ: 19
+// ที่อยู่ปัจจุบันเลขที่: 99/99
+// หมู่ที่: 88
+// ตำบล/แขวง: อัศวินรัตติกาล
+// อำเภอ/เขต: แอเรีย51
+// จังหวัด: ผู้กล้า
+// รหัสไปรษณีย์: 12080
+// อีเมล: suuuu@gmail.com
+// มือถือ: rog
+// เบอร์: 911
